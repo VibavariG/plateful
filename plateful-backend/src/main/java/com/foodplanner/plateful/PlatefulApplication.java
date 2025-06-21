3e6907e68ba17289fc47a84b5c35c0084c41a430
Vibavari Gurunathan <vibavarig@gmail.com>
2025-06-20 18:46:17 -0700

    initial backend setup

---
+++ plateful-backend/src/main/java/com/foodplanner/plateful/PlatefulApplication.java
@@ -0,0 +1,13 @@
+package com.foodplanner.plateful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatefulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatefulApplication.class, args);
+	}
+
+}
