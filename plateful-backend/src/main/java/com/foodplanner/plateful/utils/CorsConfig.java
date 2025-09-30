f8e8b5c6c0a28cb0df8bbab4140d148c65fad3bc
Vibavari Gurunathan <vibavarig@gmail.com>
2025-09-29 21:56:09 -0700

    Add cors config to connect with frontend

---
+++ plateful-backend/src/main/java/com/foodplanner/plateful/utils/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.foodplanner.plateful.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
